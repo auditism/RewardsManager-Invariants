a891bbf47cf45c14c77f78f6c49e8553d9d4decd
GiorgioDalla <giorgiodallagoiv@hotmail.com>
2025-02-05 14:49:51 +0100

    didn't compile, now it does

--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -6,7 +6,7 @@ import {BeforeAfter} from "./BeforeAfter.sol";
 import {IMint} from "./mocks/IMint.sol";
 abstract contract Properties is BeforeAfter, Asserts {
     function property__invariant() public {
-        uint256 rewards = rewardsManager.rewards(currentEpochEnd, currentVault);
+        uint256 rewards = rewardsManager.rewards(currentEpochEnd, currentVault, currentToken);
         uint256 rewardsBalance = IMint(currentToken).balanceOf(address(rewardsManager));
         t(rewardsBalance >= rewards, 'Rewards balance should always be >= reward');
         //NOTE  this could fail but who knows after how long, it would be better to track all 
